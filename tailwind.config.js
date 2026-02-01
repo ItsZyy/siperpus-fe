e63850425c4a1eb392af0cace45593d028407be8
ezy muhamad ikbal <ezymikbal@gmail.com>
2026-02-01 16:03:47 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        secondary: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+}
